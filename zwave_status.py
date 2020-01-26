691347feb412c9940c81eee65f82a9c99124f317
Robin Battey <zanfur@zanfur.com>
2020-01-26 15:17:41 -0800

    device type filtering

--- zwave_status.py
+++ zwave_status.py
@@ -10,29 +10,47 @@ import appdaemon.plugins.hass.hassapi as hass
 # and probes the device until the state of the associated light stops changing.
 #
 # To opt in to this functionality, the zwave and light entity ids must be the
-# same except for the platform name ("zwave" and "light").  This can be made
-# smarter by checking the zwave device information, also tracking other types
-# of devices, etc.
+# same except for the platform name ("zwave" and "light"), and the product
+# and manufacturer names must be in the DEVICE_TYPES class variable.  This can
+# be made smarter by turning it into an ARG.
 #
 
 
 class ZWaveStatus(hass.Hass):
-  
+
   # number of times the status has to stay the same before we drop tracking
   COUNT = 5
-  
+
   # number of polls after which we just give up and drop tracking anyway
   MAX_POLLS = 100
-  
+
+  # types of devices to do tracking for
+  DEVICE_TYPES = {
+    "HomeSeer Technologies": {"HS-WD200+ Wall Dimmer"}
+  }
+
   def initialize(self):
     self.listen_state(self.zwave_callback, "zwave", attribute="all")
     self.states = {}
     self.counts = {}
     self.tracking = {}
-    self.log("initialized")
-    
+    self.device_types = set(
+      (m, p) for m, ps in self.DEVICE_TYPES.items() for p in ps
+    )
+    self.log(
+      "initialized: tracking these device types: " +
+      ", ".join(f"{m} {p}" for m, p in self.device_types)
+    )
+
   def zwave_callback(self, entity, attribute, old, new, kwargs):
     """callback to check any zwave status updates"""
+    # make sure we care about htis device
+    manufacturer = old["attributes"]["manufacturer_name"]
+    product = old["attributes"]["product_name"]
+    if (manufacturer, product) not in self.device_types:
+      return
+
+    # look up relevant light info
     light = f"light.{self.split_entity(entity)[1]}"
     new_state = self.state(light)
     old_state = self.states.get(light, {})
